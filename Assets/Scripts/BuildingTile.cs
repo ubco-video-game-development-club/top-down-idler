11a2586dda8fef9501839d67be31eafe57a1ef98
Jaden Balogh <jadenbalogh@gmail.com>
2021-02-15 14:47:03 -0800

    Add BuildingTile

---
+++ Assets/Scripts/BuildingTile.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "New BuildingTile", menuName = "Building Tile", order = 51)]
+public class BuildingTile : Tile { }
